a010bcfeee8c68878bd2bf62b052a3ae8cf44564
Talaini <mohatala@users.noreply.github.com>
2024-01-02 12:22:39 +0100

    Create ArticleDAOTest.java

---
+++ src/test/java/com/talaini/craftwood/serviceImp/ArticleDAOTest.java
@@ -0,0 +1,32 @@
+package com.talaini.craftwood.serviceImp;
+
+import com.sun.corba.se.spi.presentation.rmi.StubAdapter;
+import com.talaini.craftwood.entity.Article;
+import com.talaini.craftwood.service.I_Article;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleDAOTest {
+
+    @Test
+    void ajouterArticle() {
+        
+    }
+
+    @Test
+    void modifierArticle() {
+    }
+
+    @Test
+    void afficherArticleAvecId() {
+    }
+
+    @Test
+    void afficherArticles() {
+    }
+
+    @Test
+    void supprimeArticle() {
+    }
+}
