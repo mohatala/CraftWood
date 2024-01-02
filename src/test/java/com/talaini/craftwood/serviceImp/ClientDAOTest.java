bbd617f5b4fcf16d05de9c3350dfe8aa25db1a21
mohamed chergaoui <61020088+MohamedChergaoui@users.noreply.github.com>
2024-01-02 15:17:09 +0100

    test

---
+++ src/test/java/com/talaini/craftwood/serviceImp/ClientDAOTest.java
@@ -0,0 +1,29 @@
+package com.talaini.craftwood.serviceImp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientDAOTest {
+
+    @Test
+    void ajouterClient() {
+    }
+
+    @Test
+    void modifierClient() {
+    }
+
+    @Test
+    void afficherClientAvecId() {
+    }
+
+    @Test
+    void afficherClients() {
+        System.out.println("rajaeeeeee");
+    }
+
+    @Test
+    void supprimeClient() {
+    }
+}
