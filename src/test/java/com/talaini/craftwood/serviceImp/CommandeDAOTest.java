5e8c0256eeff3130dd60a881b3aa0b883772d8a6
Talaini <mohatala@users.noreply.github.com>
2024-01-03 16:02:59 +0100

    Update CommandeDAOTest.java

--- src/test/java/com/talaini/craftwood/serviceImp/CommandeDAOTest.java
+++ src/test/java/com/talaini/craftwood/serviceImp/CommandeDAOTest.java
@@ -1,6 +1,7 @@
 package com.talaini.craftwood.serviceImp;
 
 import com.talaini.craftwood.config.JpaConfig;
+import com.talaini.craftwood.entity.Article;
 import com.talaini.craftwood.entity.Client;
 import com.talaini.craftwood.entity.Commande;
 import com.talaini.craftwood.entity.CommandeArticle;
@@ -23,6 +24,8 @@ class CommandeDAOTest {
     @Autowired
     CommandeDAO commandeDAO;
 
+    @Autowired
+    ArticleDAO articleDAO;
 
     @BeforeEach
     void setUp() {
@@ -36,10 +39,13 @@ class CommandeDAOTest {
     @Test
     void ajouterCommande() {
         Client client=new Client.ClientBuilder().setId_client(5).setNom("mohammed").build();
+        Article article =new Article.ArticleBuilder().setLibelle("article2").setCategorie("cat2")
+                .setPrix(500).setStock(50).build();
+        Article ar=articleDAO.ajouterArticle(article);
         Date date =new Date();
         Commande commande=new Commande.CommandeBuilder().setclient(client).setEtat("ENATTENTE")
                 .setcreated_at(date).setupdated_at(date).setTotal(500).build();
-        String s="[{'id_article':6,'libelle':'article2','categorie':'cat2','prix':500,'stock':50,'qty':1}]";
+        String s="[{'id_article':"+ar.getId_article()+",'libelle':'"+ar.getLibelle()+"','categorie':'"+ar.getCategorie()+"','prix':"+ar.getPrix()+",'stock':"+ar.getStock()+",'qty':1}]";
         assertNotNull(commandeDAO.ajouterCommande(commande,s));
     }
 
